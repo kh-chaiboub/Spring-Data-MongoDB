713ed7d9ad01f7761ef257a19fabdd18922febf8
kh-chaiboub <k.chaiboub@outlook.com>
2022-10-16 14:03:30 +0100

    Init Project Spring Data MongoDB with creating DataBase expense-tracker

---
+++ Spring-boot-mongodb/src/test/java/com/khaliddev/Springbootmongodb/SpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khaliddev.Springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
